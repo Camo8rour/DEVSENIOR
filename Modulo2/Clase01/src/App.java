c696a654266cf51d1d92d8adb001544358bfc7b9
Duvan Camilo Rodriguez Urrego <ducarour@gmail.com>
2024-12-06 23:40:14 -0500

    modulo 2 -clase 1

--- Modulo2/Clase01/src/App.java
+++ Modulo2/Clase01/src/App.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<String> students = new ArrayList<>();
+
+        
     }
 }
---
+++ Modulo2/Clase01/src/Estudiante.java
@@ -0,0 +1,16 @@
+/*public class Estudiante {
+    String nombre;
+    int edad;
+    float promedio;
+
+    //mostrarDetalles();
+    //esAprobado();
+}
+
+public static void mostrarDetalles(){
+    
+}
+
+public static void esAprobado(){
+    
+}*/
