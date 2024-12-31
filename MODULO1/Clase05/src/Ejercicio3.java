de462af8b51b3ceaa7ffda51cb52e9e135155e6f
Duvan Camilo Rodriguez Urrego <ducarour@gmail.com>
2024-12-30 23:30:33 -0500

    Progress

--- MODULO1/Clase05/src/Ejercicio3.java
+++ MODULO1/Clase05/src/Ejercicio3.java
@@ -4,6 +4,17 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         var request = new Scanner(System.in);
 
+        System.out.printf("%n\t GESTIÓN DE TAREAS%n");
+        System.out.println("1. Agregar tareas.");
+        System.out.println("2. Eliminar tareas.");
+        System.out.println("3. Mostrar tareas.");
+        System.out.println("4. Agregar tarea por pocisión.");
+        System.out.println("5. Salir.");
+        System.out.print("--> ");
+        int num = request.nextInt();
+
+        System.out.printf("%nNúmero ingresado: %d.", num);
+
         request.close();
     }
 }
