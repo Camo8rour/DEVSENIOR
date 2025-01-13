7a5977ad55125e4630a67c066587d78ca978d464
Duvan Camilo Rodriguez Urrego <ducarour@gmail.com>
2025-01-12 22:06:36 -0500

    Ejercicio 1 - Clase 6 (Avance)

--- MODULO1/Clase06/src/Ejercicio1.java
+++ MODULO1/Clase06/src/Ejercicio1.java
@@ -1,9 +1,49 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Ejercicio1 {
     public static void main(String[] args) throws Exception {
-        var request = new Scanner(System.in);
+        List<String> ventas = Arrays.asList(
+                "camisa,2,20.00",
+                "pantalon,1,35.50",
+                "zapatos,3,50.00",
+                "camisa,1,20.00",
+                "pantalon,2,35.50");
 
-        request.close();
+        //Listas con nuevos datos.
+        List<String> productos = new ArrayList<>();
+        List<Integer> cantidades = new ArrayList<>();
+        List<Double> precios_unitario = new ArrayList<>();
+
+        //Separador de datos.
+        for (String datos : ventas){
+            String[] division = datos.split(",");
+
+            String producto = division[0];
+            int cantidad = Integer.parseInt(division[1]);
+            double precio_unitario = Double.parseDouble(division[2]);
+
+            productos.add(producto);
+            cantidades.add(cantidad);
+            precios_unitario.add(precio_unitario);  
+        }
+
+        //Ingreso total de ventas.
+        double total_ventas = 0;
+
+        for (double valor : precios_unitario) {
+            total_ventas =+ valor;
+        }
+
+        System.out.printf("%nIngreso total de las ventas: %d.", total_ventas);
+
+        //Ventas por productos.
+        for (int i = 0; i < productos.size(); i++) {
+            System.out.printf("%n%nCANTIDAD DE VENTAS POR PRODUCTO");
+            System.out.printf("%n%s: %d.", productos.get(i), cantidades.get(i));
+        }
+
+
     }
 }
