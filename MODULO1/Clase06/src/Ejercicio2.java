faa00e96f1fd388cc81e2ebf1fc230ba2ec3eba8
Duvan Camilo Rodriguez Urrego <ducarour@gmail.com>
2025-01-14 23:50:13 -0500

    Avance ejercicio 2

--- MODULO1/Clase06/src/Ejercicio2.java
+++ MODULO1/Clase06/src/Ejercicio2.java
@@ -1,9 +1,66 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        var request = new Scanner(System.in);
+        List<String> calificaciones = Arrays.asList(
+                "Juan,Matematicas,85",
+                "Ana,Historia,90",
+                "Pedro,Matematicas,70",
+                "Juan,Historia,75",
+                "Ana,Matematicas,95",
+                "Pedro,Historia,80");
 
-        request.close();
+        calculateAverage(calificaciones); // Calcula el promedio.
+
+        studentsSubject(calificaciones); // Estudiantes por materia.
+
+        ratingsFilter(calificaciones); // Filtro de calificaciones mayores a el valor.
+
+        subjectHighestAverage(calificaciones); // Materia con el promedio mas alto.
+    }
+
+    private static void calculateAverage(List<String> calificaciones){
+        Map<String, Double> qualificationStudents = new HashMap<>();
+
+        for (String qualification : calificaciones) {
+            String[] division = qualification.split(",");
+            String estudiante = division[0];
+            double calificacion = Integer.parseInt(division[2]);
+            qualificationStudents.put(estudiante, qualificationStudents.getOrDefault(estudiante, (double) 0) + calificacion);
+        }
+
+        System.out.printf("%n%nPROMEDIO DE ESTIDIANTES");
+        for (String clave : qualificationStudents.keySet()) {
+            System.out.printf("%n%s: %.1f.", clave, qualificationStudents.get(clave) / 2);
+        }
+    }
+
+    private static void studentsSubject(List<String> calificaciones){
+        
+    }
+
+    private static void ratingsFilter(List<String> calificaciones){
+        int specificAmount = 80;
+        List<String> filter = new ArrayList<>();
+
+        for (String qualifications : calificaciones) {
+            String[] division = qualifications.split(",");
+            int calificacion = Integer.parseInt(division[2]);
+
+            if (calificacion > specificAmount) {
+                filter.add(qualifications);
+            }
+        }
+
+        System.out.printf("%n%nCALIFICACIONES MAYORES A %s.%n", specificAmount);
+        System.out.println(filter);
+    }
+
+    private static void subjectHighestAverage(List<String> calificaciones){
+
     }
 }
